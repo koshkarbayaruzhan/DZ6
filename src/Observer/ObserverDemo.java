eaa0dbe198c55a771010198f0ac021f8d3e83bec
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:56:33 +0500

    Update ObserverDemo.java

--- src/Observer/ObserverDemo.java
+++ src/Observer/ObserverDemo.java
@@ -1,4 +1,26 @@
-package Observer;
+package org.example.observer;
 
 public class ObserverDemo {
+
+    public static void main(String[] args) {
+
+        CurrencyExchange exchange = new CurrencyExchange();
+
+        IObserver bank = new BankObserver();
+        IObserver trader = new TraderObserver();
+        IObserver mobile = new MobileAppObserver();
+
+        exchange.attach(bank);
+        exchange.attach(trader);
+        exchange.attach(mobile);
+
+        System.out.println("Установка курса: 450.5");
+        exchange.setRate(450.5);
+
+        System.out.println("\nУдаляем трейдера\n");
+        exchange.detach(trader);
+
+        System.out.println("Установка курса: 470.0");
+        exchange.setRate(470.0);
+    }
 }
