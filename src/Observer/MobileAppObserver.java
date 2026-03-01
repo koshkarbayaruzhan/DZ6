aadeca7cc5c83093dcdd2772cbe65595d4fc7005
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:56:16 +0500

    Update MobileAppObserver.java

--- src/Observer/MobileAppObserver.java
+++ src/Observer/MobileAppObserver.java
@@ -1,4 +1,9 @@
-package Observer;
+package org.example.observer;
 
-public class MobileAppObserver {
+public class MobileAppObserver implements IObserver {
+
+    @Override
+    public void update(double rate) {
+        System.out.println("Мобильное приложение обновило курс: " + rate);
+    }
 }
