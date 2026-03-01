17186f9dced0dd7b28c734021de32c51cd0d792d
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:53:24 +0500

    Update CryptoPayment.java

--- src/Strategy/CryptoPayment.java
+++ src/Strategy/CryptoPayment.java
@@ -1,4 +1,8 @@
-package Strategy;
+package org.example.strategy;
 
-public class CryptoPayment {
+public class CryptoPayment implements IPaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата криптовалютой: " + amount + " ₸");
+    }
 }
