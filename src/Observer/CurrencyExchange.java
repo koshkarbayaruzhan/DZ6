10ec1d18bd6bbc0afb758725f1e4473403bc8103
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:55:26 +0500

    Update CurrencyExchange.java

--- src/Observer/CurrencyExchange.java
+++ src/Observer/CurrencyExchange.java
@@ -1,4 +1,32 @@
-package Observer;
+package org.example.observer;
 
-public class CurrencyExchange {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CurrencyExchange implements ISubject {
+
+    private final List<IObserver> observers = new ArrayList<>();
+    private double rate;
+
+    public void setRate(double rate) {
+        this.rate = rate;
+        notifyObservers();
+    }
+
+    @Override
+    public void attach(IObserver observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void detach(IObserver observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (IObserver observer : observers) {
+            observer.update(rate);
+        }
+    }
 }
