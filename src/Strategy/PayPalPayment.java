a02d2844bf850fcf15e2b634ced0b68c338bccf2
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:53:08 +0500

    Update PayPalPayment.java

--- src/Strategy/PayPalPayment.java
+++ src/Strategy/PayPalPayment.java
@@ -1,4 +1,8 @@
-package Strategy;
+package org.example.strategy;
 
-public class PayPalPayment {
+public class PayPalPayment implements IPaymentStrategy {
+    @Override
+    public void pay(double amount) {
+        System.out.println("Оплата через PayPal: " + amount + " ₸");
+    }
 }
