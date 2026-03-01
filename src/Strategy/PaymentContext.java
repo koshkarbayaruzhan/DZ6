7e720ba72df7c9659ccf89c09c09053dada83242
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:53:42 +0500

    Update PaymentContext.java

--- src/Strategy/PaymentContext.java
+++ src/Strategy/PaymentContext.java
@@ -1,4 +1,14 @@
-package Strategy;
+package org.example.strategy;
 
 public class PaymentContext {
+
+    private IPaymentStrategy strategy;
+
+    public void setStrategy(IPaymentStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void executePayment(double amount) {
+        strategy.pay(amount);
+    }
 }
