dcad172522b6b132407f136d5fb3f6621e434e04
koshkarbayaruzhan <kuninovaa@gmail.com>
2026-03-01 20:55:59 +0500

    Update TraderObserver.java

--- src/Observer/TraderObserver.java
+++ src/Observer/TraderObserver.java
@@ -1,4 +1,9 @@
-package Observer;
+package org.example.observer;
 
-public class TraderObserver {
+public class TraderObserver implements IObserver {
+
+    @Override
+    public void update(double rate) {
+        System.out.println("Трейдер анализирует курс: " + rate);
+    }
 }
